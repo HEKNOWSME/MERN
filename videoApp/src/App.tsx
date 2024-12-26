9444d9bde98d6d0b4a4763fff33cbd82807165af
HEKNOWSME <iranziclaude@hotmail.com>
2024-12-26 13:40:55 +0200

    add message

--- videoApp/src/App.tsx
+++ videoApp/src/App.tsx
@@ -3,16 +3,28 @@ import AllTable from "./components/Movies/AllMovies";
 import movies from "./services/fakeMovieService";
 const App = () => {
 	const [allMovies, setMovies] = useState(movies);
+	const [messages, setMessage] = useState({
+		deleted: false,
+		edited: false,
+		added: false,
+	});
 	let filteredMovies = allMovies.filter((movie) => movie);
 	const handleDelete = (id: string) => {
 		filteredMovies = filteredMovies.filter((movie) => movie._id !== id);
 		setMovies(filteredMovies);
+		setMessage({ ...messages, deleted: true });
+		setTimeout(() => {
+			setMessage({ ...messages, deleted: false });
+		}, 2000);
 	};
 	return (
 		<div className="grid">
 			<aside className="side-bar">box side</aside>
 			<nav className="nav-bar">box nav</nav>
 			<main className="main">
+				{messages.deleted && (
+					<span className="alert alert-primary">user deleted</span>
+				)}
 				<AllTable items={filteredMovies} onDelete={handleDelete} />
 			</main>
 			<footer className="footer">Footer</footer>
