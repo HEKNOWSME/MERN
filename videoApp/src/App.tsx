aaaea42012ccd2c602baa270ba80b1e47305a8c7
HEKNOWSME <iranziclaude@hotmail.com>
2024-12-26 11:11:28 +0200

    making layout for project

--- videoApp/src/App.tsx
+++ videoApp/src/App.tsx
@@ -1,7 +1,12 @@
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+	return (
+		<div className="container grid">
+			<aside className="side-bar">box side</aside>
+			<nav className="nav-bar">box nav</nav>
+      <main className="main">lorem100 </main>
+      <footer className="footer">Footer</footer>
+		</div>
+	);
+};
 
-export default App
+export default App;
