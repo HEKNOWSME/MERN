19cf06819ec872e85cfc10abd7ab9dbf764165a4
HEKNOWSME <iranziclaude@hotmail.com>
2025-01-07 14:02:13 +0200

    install express and nodemon

--- sharefy/server/app.js
+++ sharefy/server/app.js
@@ -1,12 +1,12 @@
-const express = require('express')
-const app = express()
-
-
-
-
+const express = require('express');
+const app = express();
+app.use(express.json());
+app.get('/', (req, res) => {
+  res.send('GET request to the homepage')
+})
 
 
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`shareFy listening on port ${port}!`))
