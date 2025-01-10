920dffebf17ae588dd5813dbada0f25b5c547c60
HEKNOWSME <iranziclaude@hotmail.com>
2025-01-10 21:49:50 +0200

    Finish Model

---
+++ sharefy/client/src/places/components/Model.tsx
@@ -0,0 +1,28 @@
+import "./PopUp.css";
+interface Props {
+	placeTitle: string;
+	imageUrl: string;
+	description: string;
+	onClose: () => void;
+}
+const PopUp = ({ placeTitle, imageUrl, onClose, description }: Props) => {
+	return (
+		<div className="card position-absolute top-50 start-50 translate-middle w-50 bg-body-secondary">
+			<div className="card-header text-center d-flex justify-content-between">
+				<span>{placeTitle}</span>
+				<button
+					type="button"
+					className="btn btn-close"
+					onClick={() => onClose()}
+					aria-label="close"
+				></button>
+			</div>
+			<div className="card-body">
+				<img src={imageUrl} alt="" className="card-img" />
+			</div>
+			<div className="card-footer text-center">{description}</div>
+		</div>
+	);
+};
+
+export default PopUp;
--- sharefy/client/src/places/components/PlacesList.tsx
+++ sharefy/client/src/places/components/PlacesList.tsx
@@ -1,40 +1,95 @@
 import { Link } from "react-router-dom";
 import { Place } from "../pages/Places";
 import "./places.css";
+import PopUp from "./Model";
+import { useState } from "react";
 export const PlacesList = ({ places }: { places: Place[] }) => {
+	const [isShowMap, setMap] = useState(false);
+	const [viewedPlace, setPlace] = useState<Place>({
+		id: "",
+		address: "",
+		description: "",
+		creator: "",
+		location: { latitude: "", longitude: "" },
+		title: "",
+		imageUrl: "",
+	});
+	const handleViewMap = (place: Place) => {
+		setPlace({
+			...viewedPlace,
+			title: place.title,
+			description: place.description,
+			id: place.id,
+			imageUrl: place.imageUrl,
+			address: place.address,
+		});
+		setMap(true);
+	};
+	const handleOnclose = () => {
+		setMap(false);
+	};
 	if (places.length === 0)
 		return (
 			<div className="card text-center p-3 fw-bolder">No Place Available</div>
 		);
 	return (
-		<section className="places mt-1">
-			{places.map((place) => (
-				<article className="card" key={place.id}>
-					<img
-						src={place.imageUrl}
-						className="card-image"
-						alt={place.address}
-					/>
-					<div className="card-body">
-						<span> {place.title}</span>
-						<span> {place.address}</span>
-						<span> {place.description}</span>
-					</div>
-					<div className="card-footer d-flex gap-2 justify-content-center">
-						<button type="button" className="btn btn-outline-primary">
-							View On Map
-						</button>
-						<button type="button" className="btn btn-outline-secondary">
-							<Link to={`/places/${place.id}`} className="nav-link">
-								Edit
-							</Link>
-						</button>
-						<button type="button" className="btn btn-outline-danger">
-							Delete
-						</button>
-					</div>
-				</article>
-			))}
-		</section>
+		<div className="position-relative">
+			<section className={`places mt-1 z-0 ${isShowMap && "isOpened"}`}>
+				{places.map((place) => (
+					<article className="card" key={place.id}>
+						<img
+							src={place.imageUrl}
+							className="card-image"
+							alt={place.address}
+						/>
+						<div className="card-body">
+							<p> {place.title}</p>
+							<p> {place.address}</p>
+							<span> {place.description}</span>
+						</div>
+						<div className="card-footer d-flex gap-2 justify-content-center">
+							<button
+								type="button"
+								className="btn btn-outline-primary"
+								disabled={isShowMap}
+								onClick={() => {
+									handleViewMap(place);
+								}}
+							>
+								View On Map
+							</button>
+							<button
+								type="button"
+								className="btn btn-outline-secondary"
+								disabled={isShowMap}
+							>
+								<Link
+									to={`/places/${place.id}`}
+									className="nav-link"
+									disabled={isShowMap}
+								>
+									Edit
+								</Link>
+							</button>
+							<button
+								type="button"
+								className="btn btn-outline-danger"
+								disabled={isShowMap}
+							>
+								Delete
+							</button>
+						</div>
+					</article>
+				))}
+			</section>
+			{isShowMap && (
+				<PopUp
+					placeTitle={viewedPlace.address}
+					imageUrl={viewedPlace.imageUrl}
+					description={viewedPlace.description}
+					onClose={handleOnclose}
+				/>
+			)}
+		</div>
 	);
 };
